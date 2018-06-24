ca0b40170b0bcf877156452d98bfe34da7ddd973
Abhijit <abhijit.gupta1122@gmail.com>
2018-06-24 16:17:32 -0400

    Complete. No Orientation Support.

--- app/src/main/java/ga/abhijit/dyce/MainActivity.java
+++ app/src/main/java/ga/abhijit/dyce/MainActivity.java
@@ -2,8 +2,12 @@ package ga.abhijit.dyce;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,8 +17,34 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button rollMe = (Button) findViewById(R.id.Dyce_button);
-        ImageView leftDice = (ImageView) findViewById(R.id.image_leftDice);
-        ImageView rightDice = (ImageView) findViewById(R.id.image_rightDice);
+        final ImageView leftDice = (ImageView) findViewById(R.id.image_leftDice);
+        final ImageView rightDice = (ImageView) findViewById(R.id.image_rightDice);
+        //We need to make sure that this stays constant in order to use it in the Event Listener.
+        final int[] diceArray = {R.drawable.dice1,
+                R.drawable.dice2,
+                R.drawable.dice3,
+                R.drawable.dice4,
+                R.drawable.dice5,
+                R.drawable.dice6};
+
+        rollMe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Log.d("dyce", "Button Pressed!");
+                Random randNumGen = new Random();
+
+                //random from 0->5
+                int rand = randNumGen.nextInt(6);
+                Log.d("Random", "Boiya"+rand);
+                leftDice.setImageResource(diceArray[rand]);
+
+                rand = randNumGen.nextInt(6);
+                Log.d("Random", "Boiya"+rand);
+                rightDice.setImageResource(diceArray[rand]);
+            }
+        });
+
+
 
 
 
