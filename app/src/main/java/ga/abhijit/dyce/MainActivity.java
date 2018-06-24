ae08c2e6ceeecc6e036126f53bb7feb833ea13f4
Abhijit <abhijit.gupta1122@gmail.com>
2018-06-24 15:53:14 -0400

    Design Layout Updated

--- app/src/main/java/ga/abhijit/dyce/MainActivity.java
+++ app/src/main/java/ga/abhijit/dyce/MainActivity.java
@@ -2,6 +2,8 @@ package ga.abhijit.dyce;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button rollMe = (Button) findViewById(R.id.Dyce_button);
+        ImageView leftDice = (ImageView) findViewById(R.id.image_leftDice);
+        ImageView rightDice = (ImageView) findViewById(R.id.image_rightDice);
+
+
+
+
     }
 }
